a0a61bf85a281fc0a022971820b5b78ef7b7761e
Charles Stafford <j.stafford1992@gmail.com>
2017-01-06 12:59:23 -0700

    setting up knex table

--- migrations/20170103162319_members.js
+++ migrations/20170103162319_members.js
@@ -1,9 +1,13 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-
+  knex.schema.createTable('members', function(table) {
+    table.incrememts();
+    table.string('name');
+    table.timestamps();
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  knex.schema.dropTable('members');
 };
